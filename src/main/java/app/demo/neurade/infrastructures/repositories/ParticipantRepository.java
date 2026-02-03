a4057f6ba6ecd4d1a219fdbbb3a279690747e6ca
BananaOnTheTree <124771692+BananaOnTheTree@users.noreply.github.com>
2026-02-03 12:22:49 +0700

    change: fix N+1 problem in ClassServiceImpl.addParticipants

--- src/main/java/app/demo/neurade/infrastructures/repositories/ParticipantRepository.java
+++ src/main/java/app/demo/neurade/infrastructures/repositories/ParticipantRepository.java
@@ -2,11 +2,20 @@ package app.demo.neurade.infrastructures.repositories;
 
 import app.demo.neurade.domain.models.ClassParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public interface ParticipantRepository extends JpaRepository<ClassParticipant, UUID> {
     List<ClassParticipant> findAllByClazz_Id(Long clazzId);
     boolean existsByClazz_IdAndUser_Id(Long clazzId, Long userId);
+
+    @Query("""
+        select p from ClassParticipant p
+        where p.clazz.id = :clazzId
+        and p.user.id in :userIds
+    """)
+    Set<Long> findExistingUserIdsInClass(Long clazzId, List<Long> userIds);
 }
